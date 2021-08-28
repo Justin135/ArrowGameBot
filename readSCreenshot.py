04662199019843f181b924b0c19acc828d1a5c31
Justin135 <justin.bornais@gmail.com>
2021-08-27 23:34:42 -0400

    Added basic text recognition and screenshotting.

--- readSCreenshot.py
+++ readSCreenshot.py
@@ -2,9 +2,37 @@
 import cv2 # An image proccessing library
 import pytesseract # an image to text library
 import numpy as np # used for mathematics but can be used in image proccessing
+import pyautogui
+import time
+import pyscreenshot
+
+# Positions:
+# Top Left: x=750, y=365
+# Top Right: x=1165, y=365
+# Bottom Left: x=750, y=794
+# Bottom Right: x=1165, y=794
 
 # Configure the module
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\Justin\\AppData\\Local\\Tesseract-OCR\\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+x_coords = [795, 897, 1000, 1104]
+y_coords = [405, 510, 610, 715]
+
+for i in range(4):
+    
+    for j in range(4):
+        image = pyscreenshot.grab(bbox=(x_coords[j], y_coords[i], x_coords[j] + 20, y_coords[i] + 40)) # Capture the screen.
+        image.save("screenshots/" + str((i * 4) + j) + ".png")
+
+# image = pyscreenshot.grab(bbox=(750, 365, 1165, 794)) # Capture the screen.
+image = pyscreenshot.grab(bbox=(795, 400, 820, 440)) # Capture the screen.
+image.save("screen.png") # To save the screenshot
+img = cv2.imread("screen.png")
+a1 = pytesseract.image_to_string(img, config="--psm 13")[0] # Works for only one number.
+print(a1)
+
+
+"""
 
 # Make the image grey
 img = cv2.imread('your_img.png')
@@ -18,3 +46,4 @@ img = cv2.dilate(img, kernel, iterations=1)
 out_below = pytesseract.image_to_string(img)
 # Print the text
 print(out_below)
+"""
